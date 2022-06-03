a0cd4db6998bebc5dcf9ec0ecd02abc3ca6f2e44
Piotr Materna <piotr.materna0@gmail.com>
2022-06-03 20:35:54 +0200

    added functionality, deposit and withdrawal of material

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,8 @@
 const searchBox = document.querySelector('.searchbox')
-
 const btnWithdraw = document.querySelectorAll('.withdraw')
+const btnDeposit = document.querySelectorAll(`.deposit`)
 
+// const btnWithdrawSingle = btnWithdraw.map()
 const trItem = document.querySelectorAll(`.item`)
 
 const searchEngine = e => {
@@ -17,5 +18,26 @@ const searchEngine = e => {
 		}
 	})
 }
+
+btnWithdraw.forEach(element => {
+	element.addEventListener(`click`, () => {
+		let row = element.closest(`tr`).querySelector(`.amount`)
+		// let rowAmount = row.querySelector(`.amount`)
+		let amount = element.closest(`tr`).querySelector(`.input-amount`)
+		let firstNumber = Number(row.innerHTML)
+		// let secondNumber = Number(amount.value)
+		row.innerHTML = Number(row.innerHTML) - Number(amount.value)
+	})
+})
+
+btnDeposit.forEach(element => {
+	element.addEventListener(`click`, () => {
+		let row = element.closest(`tr`).querySelector(`.amount`)
+		let amount = element.closest(`tr`).querySelector(`.input-amount`)
+		let firstNumber = Number(row.innerHTML)
+		// let secondNumber = Number(amount.value)
+		row.innerHTML = Number(row.innerHTML) + Number(amount.value)
+	})
+})
 
 searchBox.addEventListener('keyup', searchEngine)
