25ce85e4c3df13fbfd2a47b92840b8d21268dfe5
Piotr Materna <piotr.materna0@gmail.com>
2022-06-03 17:14:07 +0200

    fixed styling issues

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,16 +2,16 @@ const searchBox = document.querySelector('.searchbox')
 
 const btnWithdraw = document.querySelectorAll('.withdraw')
 
-const tr = document.querySelectorAll(`tr`)
+const trItem = document.querySelectorAll(`.item`)
 
-const searchEngine = e => {     
+const searchEngine = e => {
 	const text = e.target.value.toLowerCase()
 
-	tr.forEach(element => {
+	trItem.forEach(element => {
 		const task = element.textContent
 
 		if (task.toLowerCase().indexOf(text) !== -1) {
-			element.style.display = 'inline'
+			element.style.display = 'table-row'
 		} else {
 			element.style.display = 'none'
 		}
